9d4515e2e71834018c488d60a4c93d9e3e926ed5
Taylor Lee <lisu60g@gmail.com>
2017-01-02 15:59:42 +0800

    Replaced line with triangle in test

--- main.cpp
+++ main.cpp
@@ -5,14 +5,15 @@
 using namespace Eigen;
 
 int main() {
-    GeoUtils::GeoElement line;
-    line.push_back(Vector2d(2, 5));
-    line.push_back(Vector2d(3, 1));
+    GeoUtils::GeoElement triangle;
+    triangle.push_back(Vector2d(2, 5));
+    triangle.push_back(Vector2d(3, 1));
+    triangle.push_back(Vector2d(2, 9));
     Vector2d movement(3, -5);
-    std::cout<<"original line:"<<std::endl;
-    GeoUtils::printElement(line);
+    std::cout<<"original triangle:"<<std::endl;
+    GeoUtils::printElement(triangle);
     std::cout<<"movement vector:"<<std::endl<<movement<<std::endl;
-    std::cout<<"moved line:"<<std::endl;
-    GeoUtils::printElement(GeoUtils::move(movement, line));
+    std::cout<<"moved triangle:"<<std::endl;
+    GeoUtils::printElement(GeoUtils::move(movement, triangle));
     return 0;
 }
